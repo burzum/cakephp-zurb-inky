e56c0605e516d224012464be50ae3a2dbafe44c8
Florian Krämer <florian.kraemer@kreative-design.net>
2018-03-20 10:59:14 +0100

    Adding a MailHelper

---
+++ src/Helper/MailHelper.php
@@ -0,0 +1,36 @@
+<?php
+namespace Burzum\ZurbInky\View\Helper;
+
+use Cake\View\Helper\HtmlHelper;
+
+/**
+ * Mail Helper
+ */
+class MailHelper extends HtmlHelper {
+
+	public function initialize(array $config) {
+		parent::initialize($config);
+	}
+
+	public function setDefaultUrlParams($params, $merge = true) {
+		$this->setConfig('urlParams', $params, $merge);
+	}
+
+	public function link($title, $url = null, array $options = []) {
+		if (is_array($url)) {
+			if (!isset($url['?'])) {
+				$url['?'] = [];
+			}
+			if (isset($options['urlParams'])) {
+				if (is_array($options['urlParams'])) {
+					$url['?'] = array_merge($url['?'], $options['urlParams']);
+				}
+			} else {
+				$url['?'] = array_merge($url['?'], (array)$this->getConfig('urlParams'));
+			}
+		}
+
+		return parent::link($title, $url, $options);
+	}
+
+}
